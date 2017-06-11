afdff10f435357d54b51a2390a5f0224dd9af2e4
vsoaresbraz <vsoaresbraz@gmail.com>
2017-06-11 18:37:30 -0300

    Lista_de_Compras

---
+++ js/config.js
@@ -0,0 +1,9 @@
+function setConfig() {
+  var texts = {
+    "title": "Controle de Compras"
+  };
+  document.title = texts.title;
+  document.getElementById("navTitle").innerHTML = texts.title;
+}
+
+setConfig();
